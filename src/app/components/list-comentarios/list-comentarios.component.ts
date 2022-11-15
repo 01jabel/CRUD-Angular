908c1e373168def3caba4cd1dc04f623df7772d5
Ramon Polanco <eskinatecnologica@gmail.com>
2022-11-15 00:14:32 -0400

    Firt Commit

---
+++ src/app/components/list-comentarios/list-comentarios.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import { Comentario } from 'src/app/interfaces/Comentario';
+
+@Component({
+  selector: 'app-list-comentarios',
+  templateUrl: './list-comentarios.component.html',
+  styleUrls: ['./list-comentarios.component.css']
+})
+export class ListComentariosComponent implements OnInit {
+
+  listComentarios: Comentario[] = [
+    { titulo: 'Angular', creador: 'Jabel', fechaCreacion: new Date(), texto: 'Framework para crear SPA'},
+    { titulo: 'React', creador: 'Oneal', fechaCreacion: new Date(), texto: 'Libreria para crear SPA'}
+  ]
+
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
